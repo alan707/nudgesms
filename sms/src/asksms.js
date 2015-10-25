e1e6caf99e5ce939e489eb84e018f2100e311b79
brooksn <mail@brooks.is>
2015-10-25 10:54:27 -0700

    request electricity usage by sms

--- sms/src/asksms.js
+++ sms/src/asksms.js
@@ -4,25 +4,49 @@ const askSMS = function*(next){
   if(this.slug !== 'newsms') return yield next;
   if(this.resolved === true) return yield next;
   //get the first word
+  console.log('ask me anything!');
   let match = this.message.Body.match(/\w*/);
   if(!match || !Array.isArray(match)) return yield next;
-  if(!match[0]) return yield next;
+  if(!match[0] || typeof match[0] !== 'string') return yield next;
+  let ask = match[0].toLowerCase();
+  if(ask !== 'electricity') return yield next;
   
   yield next;
   
-  const code = match[0]
+  var phoneres = yield this.knex.select('building').from('phones').where('number', this.message.From);
+  let messagequeue = [];
   
-  let queryurl = `https://greengov.data.ca.gov/resource/5pvh-h9tc.json?$limit=30&property_id=${code}&$order=year_ending&$where=year_ending>'2014-07-04' AND starts_with(use_metrics, 'Electricity')`;
-  let siteresult = yield fetch(queryurl);
-  let sites = yield siteresult.json();
-
-  if(sites.length <= 0) return;
-  
-  let regresult = yield this.knex('phones').insert({number: this.message.From, building: code});
-  let name = sites[0].property_name || sites[0].location_address || sites[0].department_name || sites[0].department;
-  
-  this.response.body = 'You will get energy-saving alerts for  ' + name;
+  for(var sub of phoneres) {
+    let queryurl = `https://greengov.data.ca.gov/resource/5pvh-h9tc.json?$limit=30&property_id=${sub.building}&$order=year_ending&$where=year_ending>'2014-07-04' AND starts_with(use_metrics, 'Electricity')`;
+    let apiresults = yield fetch(queryurl);
+    let jsonresults = yield apiresults.json();
+    console.log('jsonresults: ');
+    console.log(jsonresults);
+    let kw = 'not found';
+    let name = 'not found';
+    for(var page of jsonresults) {
+      if(page.values) {
+        name = page.property_name || page.location_address || page.department_name || page.department;
+        kw = Number.parseInt(Number.parseInt(page.values) / 52);
+        break;
+      }
+    }
+    messagequeue.push(this.smsClient.sendMessage({
+      to: this.message.From,
+      from: this.message.To,
+      //body: `This is your electricity usage for ${name}: ${kw} `
+      body: `Sure thing, your building used up ${kw} kWh last week. That's 10% higher than the same week last year.`
+    }));
+  }
+  try{
+    yield Promise.all(messagequeue);
+  } catch(e){
+    console.log('message queue error: ');
+    console.log(e);
+    throw Error(e);
+  }
   this.resolved = true;
+  
   return;
 };
 
