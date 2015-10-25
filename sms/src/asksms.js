34e0b7f8150c777e057defd83e5be4a97a1cacf6
brooksn <mail@brooks.is>
2015-10-25 11:03:15 -0700

    ask for water usage by sms

--- sms/src/asksms.js
+++ sms/src/asksms.js
@@ -9,7 +9,18 @@ const askSMS = function*(next){
   if(!match || !Array.isArray(match)) return yield next;
   if(!match[0] || typeof match[0] !== 'string') return yield next;
   let ask = match[0].toLowerCase();
-  if(ask !== 'electricity') return yield next;
+  let metric = '';
+  let units = '';
+  if (ask === 'electricity') {
+    metric = 'Electricity';
+    units = 'kWh';
+  } else if(ask === 'water') {
+    metric = 'Water';
+    units = 'gallons';
+  } else {
+    return yield next;
+  }
+  //if(ask !== 'electricity') return yield next;
   
   yield next;
   
@@ -17,7 +28,7 @@ const askSMS = function*(next){
   let messagequeue = [];
   
   for(var sub of phoneres) {
-    let queryurl = `https://greengov.data.ca.gov/resource/5pvh-h9tc.json?$limit=30&property_id=${sub.building}&$order=year_ending&$where=year_ending>'2014-07-04' AND starts_with(use_metrics, 'Electricity')`;
+    let queryurl = `https://greengov.data.ca.gov/resource/5pvh-h9tc.json?$limit=30&property_id=${sub.building}&$order=year_ending&$where=year_ending>'2014-07-04' AND starts_with(use_metrics, '${metric}')`;
     let apiresults = yield fetch(queryurl);
     let jsonresults = yield apiresults.json();
     console.log('jsonresults: ');
@@ -35,7 +46,7 @@ const askSMS = function*(next){
       to: this.message.From,
       from: this.message.To,
       //body: `This is your electricity usage for ${name}: ${kw} `
-      body: `Sure thing, your building used up ${kw} kWh last week. That's 10% higher than the same week last year.`
+      body: `Sure thing, your building used up ${kw} ${units} last week. That's 10% higher than the same week last year.`
     }));
   }
   try{
